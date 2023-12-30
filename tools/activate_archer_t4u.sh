c2e640498086109746c12a8db0a02973be8a7be9
lcontreras <leonardo.contrerasv@gmail.com>
2023-12-30 20:24:47 -0300

    Add a folder for tools

---
+++ tools/activate_archer_t4u.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Activate TP-Link Wireless adapter Archer T4U
+#
+
+#Colors
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  greenColour="\e[0;32m\033[1m"
+  endColour="\033[0m\e[0m"
+  redColour="\e[0;31m\033[1m"
+  blueColour="\e[0;34m\033[1m"
+  yellowColour="\e[0;33m\033[1m"
+  purpleColour="\e[0;35m\033[1m"
+  turquoiseColour="\e[0;36m\033[1m"
+  grayColour="\e[0;37m\033[1m"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  greenColour="\x1B[0;32m\033[1m"
+  endColour="\033[0m\x1B[0m"
+  redColour="\x1B[0;31m\033[1m"
+  blueColour="\x1B[0;34m\033[1m"
+  yellowColour="\x1B[0;33m\033[1m"
+  purpleColour="\x1B[0;35m\033[1m"
+  turquoiseColour="\x1B[0;36m\033[1m"
+  grayColour="\x1B[0;37m\033[1m"
+else
+  #No colors
+  greenColour=""
+  endColour=""
+  redColour=""
+  blueColour=""
+  yellowColour=""
+  purpleColour=""
+  turquoiseColour=""
+  grayColour="" 
+fi
+
+# Need to be root
+
+if [ "$(id -u)" == "0" ]; then
+    echo -e "\n${yellowColour}[*]${endColour}${grayColour} Installing driver for${endColour}${blueColour} TP-Link Archer T4U... ${endColour}"
+    sudo apt install -y realtek-rtl88xxau-dkms > /dev/null 2>&1
+
+    if [[ "$(echo $?)" == "0" ]]; then
+        sudo modprobe -a 88XXau > /dev/null 2>&1
+        echo -e "\n${greenColour}[*]${endColour} Driver installed success!"
+    else
+        echo -e "\n${redColour}[!]${endColour} Error installing driver!"
+    fi
+else
+    echo -e "\n${redColour}[!] Stop! You need to be root${endColour}\n"
+fi
---
+++ tools/install_brave.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Install Brave web browser
+#
+
+#Colors
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  greenColour="\e[0;32m\033[1m"
+  endColour="\033[0m\e[0m"
+  redColour="\e[0;31m\033[1m"
+  blueColour="\e[0;34m\033[1m"
+  yellowColour="\e[0;33m\033[1m"
+  purpleColour="\e[0;35m\033[1m"
+  turquoiseColour="\e[0;36m\033[1m"
+  grayColour="\e[0;37m\033[1m"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  greenColour="\x1B[0;32m\033[1m"
+  endColour="\033[0m\x1B[0m"
+  redColour="\x1B[0;31m\033[1m"
+  blueColour="\x1B[0;34m\033[1m"
+  yellowColour="\x1B[0;33m\033[1m"
+  purpleColour="\x1B[0;35m\033[1m"
+  turquoiseColour="\x1B[0;36m\033[1m"
+  grayColour="\x1B[0;37m\033[1m"
+else
+  #No colors
+  greenColour=""
+  endColour=""
+  redColour=""
+  blueColour=""
+  yellowColour=""
+  purpleColour=""
+  turquoiseColour=""
+  grayColour="" 
+fi
+
+# Need to be root
+
+if [ "$(id -u)" == "0" ]; then
+    echo -e "\n${yellowColour}[*]${endColour}${grayColour} Installing ${endColour}${blueColour} Brave Browser... ${endColour}"
+    curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave4u7jddbv7cyviptqjc7jusxh72uik7zt6adtckl5f4nwy2v72qd.onion/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+
+    if [[ "$(echo $?)" == "0" ]]; then
+        echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave4u7jddbv7cyviptqjc7jusxh72uik7zt6adtckl5f4nwy2v72qd.onion/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
+        
+        apt update > /dev/null 2>&1
+        apt install -y brave-browser > /dev/null 2>&1
+        
+        if [[ "$(echo $?)" == "0" ]]; then
+          echo -e "\n${greenColour}[*]${endColour} Brave installed successfully."
+        else
+          echo -e "\n${redColour}[!]${endColour} Error installing brave!"
+        fi
+    else
+        echo -e "\n${redColour}[!]${endColour} Error downloading brave keyring!"
+    fi
+
+else
+    echo -e "\n${redColour}[!] Stop! You need to be root${endColour}\n"
+fi
